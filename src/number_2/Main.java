5672e4ce8d224c3d76cc4639423842f7be4081e4
AlexCherba <alexander.cherba@gmail.com>
2017-11-30 20:06:39 +0200

    The Task 2 is done

--- src/number_2/Main.java
+++ src/number_2/Main.java
@@ -1,6 +1,30 @@
 package number_2;
+
+import java.util.Scanner;
+
 /*
 2.Компьютер загадывает число от 1 до n. У пользователя k попыток отгадать. После каждой неудачной попытки компьютер сообщает меньше или больше загаданное число. В конце игры текст с результатом (или “Вы угадали”, или “Попытки закончились”).
 */
 public class Main {
-}
+    public static void main(String[] args) {
+        int computerNumber;
+        int customAttempts;
+        int userNumber;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Компьютер загадывает чмсло от 1 до:");
+        computerNumber = Utils.createNumber(scanner.nextInt());
+        System.out.print("Введите количество доступных попыток:");
+        customAttempts = scanner.nextInt();
+
+        for (int i = 0; i < customAttempts; i++) {
+            System.out.println("Введите число:");
+            userNumber = scanner.nextInt();
+            if (userNumber == computerNumber){
+                System.out.print("Вы угадали");
+                return;
+            }
+        }
+        System.out.println("Попытки закончились");
+    }
+ }
---
+++ src/number_2/Utils.java
@@ -0,0 +1,9 @@
+package number_2;
+
+public abstract class Utils {
+
+    public static int createNumber(int n) {
+        return 1 + (int) (Math.random() * n);
+    }
+
+}
